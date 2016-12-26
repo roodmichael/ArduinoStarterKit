9613796f58c8b03a9e6d0b3edf85826435162387
Michael Rood <rood.michael@gmail.com>
2016-12-26 13:12:30 -0800

    Added light functionality to annoying_christmas_card project.

--- annoying_xmas_card/annoying_xmas_card.ino
+++ annoying_xmas_card/annoying_xmas_card.ino
@@ -1,22 +1,44 @@
 int speakerPin = 8;
+int ledPin2 = 2;
+int ledPin3 = 3;
+int ledPin4 = 4;
 
-int length = 64;
-char notes[] = "ggdgadbabCbaggsesgabsedd DCbCbabgasedggsgagdbbabCbabCbagsgCbagg ";
-int beats[] = { 2,4,2,2,4,4,2,2,2,2,4,2,2,4,2,2,2,2,2,2,4,3,1,6,2,4,2,2,4,4,2,2,2,2,3,1,2,2,2,2,2,2,4,2,2,2,2,2,2,4,2,2,2,2,2,2,4,2,2,4,3,1,6,8 };
+int length = 54;
+char notes[] = "gfedcdecdefdedcbc gfedcdecdefdedcbc defdefgd efgaBCBag";
+int beats[] = { 2,1,2,2,2,2,2,2,1,1,1,1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,1,1,1,1,2,2,2,2,2,2,2,1,1,1,2,1,1,2,2,1,1,2,1,1,2,2,2,2 };
 int tempo = 300;
 
+void lightSwitch(bool led1, bool led2, bool led3) {
+  digitalWrite(ledPin2, led1);
+  digitalWrite(ledPin3, led2);
+  digitalWrite(ledPin4, led3);
+}
+
+void lightPattern(int tone) {
+  lightSwitch(LOW, LOW, HIGH);
+  
+  if (tone <= 1700) {
+    lightSwitch(LOW, HIGH, HIGH); 
+  }
+  
+  if (tone <= 1432) {
+    lightSwitch(HIGH, HIGH, HIGH);
+  }
+}
 void playTone(int tone, int duration) {
   for (long i = 0; i < duration * 1000L; i += tone * 2) {
     digitalWrite(speakerPin, HIGH);
+    lightPattern(tone);
     delayMicroseconds(tone);
     digitalWrite(speakerPin, LOW);
+    lightSwitch(LOW, LOW, LOW);
     delayMicroseconds(tone);
   }
 }
 
 void playNote(char note, int duration) {
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  char names[] = { 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'B', 'C' };
+  int tones[] = { 2015, 1915, 1700, 1519, 1432, 1275, 1136, 1014, 1000 };
 
   // play the tone corresponding to the note name
   for (int i = 0; i < 8; i++) {
@@ -28,11 +50,15 @@ void playNote(char note, int duration) {
 
 void setup() {
   pinMode(speakerPin, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
   for (int i = 0; i < length; i++) {
     if (notes[i] == ' ') {
+      lightSwitch(LOW, LOW, LOW);
       delay(beats[i] * tempo); // rest
     } else {
       playNote(notes[i], beats[i] * tempo);
